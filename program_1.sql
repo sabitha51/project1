b6f8d2f042f15ab1a5b4e9f318081398da0f550a
sabitha.s <132537220+sabitha51@users.noreply.github.com>
2023-07-04 10:29:56 +0530

    Add files via upload

---
+++ program_1.sql
@@ -0,0 +1,16 @@
+create database m;
+create table m.student(serialnumber int, rollnumber int , name varchar(20), department varchar(20), college varchar(20), place varchar(20));
+select *from m.student;
+insert into m.student values
+(01 ,"200044","sabitha","bme","mec","rasipuram"),
+(02 ,"200027","mouni","bme","mec","rasipuram"),
+(03 ,"200321","keerthi","bme","mec","rasipuram"),
+(04 ,"200322","keerthi","bme","mec","rasipuram"),
+(05 ,"200323","keerthi","bme","mec","rasipuram"),
+(06 ,"200324","keerthi","bme","mec","rasipuram"),
+(07 ,"200325","keerthi","bme","mec","rasipuram"),
+(08 ,"200326","keerthi","bme","mec","rasipuram"),
+(09 ,"200327","keerthi","bme","mec","rasipuram"),
+(10 ,"200328","keerthi","bme","mec","rasipuram");
+
+
